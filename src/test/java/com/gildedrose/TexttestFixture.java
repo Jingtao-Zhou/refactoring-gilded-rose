0c9b1607cd59c9942436824e8b75b1776c10df3a
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-01 17:44:46 +0800

    modify the text test

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,21 +1,26 @@
 package com.gildedrose;
 
+import com.gildedrose.item.AgedBrie;
+import com.gildedrose.item.BackstagePassesTo_a_TAFKAL80ETC_Concert;
+import com.gildedrose.item.OtherItem;
+import com.gildedrose.item.SulfurasHandOfRagnaros;
+
 public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                new OtherItem("+5 Dexterity Vest", 10, 20), //
+                new AgedBrie("Aged Brie", 2, 0), //
+                new OtherItem("Elixir of the Mongoose", 5, 7), //
+                new SulfurasHandOfRagnaros("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new SulfurasHandOfRagnaros("Sulfuras, Hand of Ragnaros", -1, 80),
+                new BackstagePassesTo_a_TAFKAL80ETC_Concert("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new BackstagePassesTo_a_TAFKAL80ETC_Concert("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new BackstagePassesTo_a_TAFKAL80ETC_Concert("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new BackstagePassesTo_a_TAFKAL80ETC_Concert("Backstage passes to a TAFKAL80ETC concert", 1, 20),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new OtherItem("Conjured Mana Cake", 3, 6) };
 
         GildedRose app = new GildedRose(items);
 
