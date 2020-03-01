d51feef716db9d8ed40a5efc3253a1aec757a253
Jingtao Zhou <jingtao.zhou@thoughtworks.com>
2020-03-01 16:01:54 +0800

    [feature envy] move update quality method into item

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -8,55 +8,8 @@ class GildedRose {
     }
 
     public void update_quality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                subQualityIfInStockExceptSulfuras(items[i]);
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sell_in < 11) {
-                            addQualityBeforeReach50(items[i]);
-                        }
-
-                        if (items[i].sell_in < 6) {
-                            addQualityBeforeReach50(items[i]);
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sell_in = items[i].sell_in - 1;
-            }
-
-            if (items[i].sell_in < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        subQualityIfInStockExceptSulfuras(items[i]);
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    addQualityBeforeReach50(items[i]);
-                }
-            }
-        }
-    }
-
-    private void addQualityBeforeReach50(Item item){
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
-    }
-
-    private void subQualityIfInStockExceptSulfuras(Item item){
-        if (item.quality > 0) {
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                item.quality = item.quality - 1;
-            }
+        for (Item item : items) {
+            item.update_quality();
         }
     }
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -14,6 +14,58 @@ public class Item {
         this.quality = quality;
     }
 
+    void update_quality() {
+        if (!this.name.equals("Aged Brie")
+                && !this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            subQualityIfInStockExceptSulfuras();
+        } else {
+            if (this.quality < 50) {
+                this.quality = this.quality + 1;
+
+                if (this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (this.sell_in < 11) {
+                        addQualityBeforeReach50();
+                    }
+
+                    if (this.sell_in < 6) {
+                        addQualityBeforeReach50();
+                    }
+                }
+            }
+        }
+
+        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+            this.sell_in = this.sell_in - 1;
+        }
+
+        if (this.sell_in < 0) {
+            if (!this.name.equals("Aged Brie")) {
+                if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    subQualityIfInStockExceptSulfuras();
+                } else {
+                    this.quality = this.quality - this.quality;
+                }
+            } else {
+                addQualityBeforeReach50();
+            }
+        }
+
+    }
+
+    private void addQualityBeforeReach50() {
+        if (this.quality < 50) {
+            this.quality = this.quality + 1;
+        }
+    }
+
+    private void subQualityIfInStockExceptSulfuras() {
+        if (this.quality > 0) {
+            if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
+                this.quality = this.quality - 1;
+            }
+        }
+    }
+
     @Override
     public String toString() {
         return this.name + ", " + this.sell_in + ", " + this.quality;
